36b335caeaaf11fc6837a6ff8ce03528ef99d7f8
Micael <avilla.micael@gmail.com>
2026-04-09 12:32:39 -0300

    feat(02-04): registrar rota GET /api/coleta/logs/ (LOG-01)
    - Adiciona path('coleta/logs/', LogColetaListView.as_view(), name='log-coleta-list')
    - Importa LogColetaListView de .views

--- backend_monitoramento/api/urls.py
+++ backend_monitoramento/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import PingView
+from .views import PingView, LogColetaListView
 from .views.usinas import UsinaViewSet
 from .views.garantias import GarantiaListView
 from .views.inversores import InversorViewSet
@@ -18,5 +18,6 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('ping/', PingView.as_view(), name='api_ping'),
     path('garantias/', GarantiaListView.as_view(), name='garantia-list'),
+    path('coleta/logs/', LogColetaListView.as_view(), name='log-coleta-list'),
     path('', include(router.urls)),
 ]
